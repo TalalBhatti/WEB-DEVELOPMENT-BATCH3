c89617f09fcc6732bcca3a1e101fca1cb8373cba
TalalBhatti <talalabhatti@gmail.com>
2023-09-18 15:29:51 +0500

    second class

---
+++ Class-1/script.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+alert("Hello");
+
+var name = "Ali";
+var age = 25;
+var gender = "Male";
+var adult = true;
+var weight = 79.5;
+
+console.log("My name is " + name + " "+ "I am " + age+ " years old");
