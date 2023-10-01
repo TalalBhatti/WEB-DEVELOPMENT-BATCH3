7dfde87d2a91ce685fe713ba8e605b9d527012f0
syedmisbahurrehman <syedmisbah588@gmail.com>
2023-10-01 16:38:13 +0500

    counter added

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import UiButton from './components/button '
+import { useState } from 'react';
 
 function App() {
+const [count, setCount] = useState(0)
+
   const toLearn = [
     { title: 'listing', id: 1 },
     { title: 'Props', id: 2 },
     { title: 'Navigation', id: 3 },
   ];
   const Lanuage = "React"
-  const toShow = toLearn.map((item)=>
+  const toShow = toLearn.map((item) =>
     <li>
       {item.title}
     </li>
   )
-  const filtered = toLearn.filter((item)=> item.id == 3)
+  const filtered = toLearn.filter((item) => item.id == 3)
   console.log(filtered)
   return (
     <div className='App'>
       <h1>Hello {Lanuage}. Im a beginner here</h1>
-      <UiButton/>
+      <h2>Count is {count}</h2>
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+      <button onClick={()=>setCount(count-1)}>Decrement</button>
+      <button onClick={()=>setCount(count==0)}>Reset</button>
+
+
+      {/* <UiButton /> */}
       <ul>{toShow}</ul>
       {filtered[0].title}
     </div>
